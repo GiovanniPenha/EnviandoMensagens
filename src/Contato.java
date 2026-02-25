14c024b08cd476588e73a1d68cf26513a6b4a3fa
GiovanniPenha <giovanni.lpenha@gmail.com>
2026-02-25 13:23:13 -0300

    Atualizando...

--- src/Contato.java
+++ src/Contato.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Contato {
+
+    void enviarMensagem(String mensagem){
+        System.out.println("Mensagem enviada para todos: " + mensagem);
+    }
+
+    void enviarMensagem(String nome, String mensagem){
+        System.out.println("Mensagem enviada para " + nome + ": " + mensagem);
+    }
+
+    void enviarMensagem(String nome, String mensagem, int repete){
+        for(int i = 1; i <= repete; i++){
+            System.out.println("Mensagem para: " + nome + ": " + mensagem);
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        Contato contato = new Contato();
+
+        contato.enviarMensagem("Olá!");
+        contato.enviarMensagem("Giovanni", "Parabéns, você foi aprovado!");
+        contato.enviarMensagem("Giuliana", "Não se esqueça de enviar os documentos!", 2);
+    }
 }
